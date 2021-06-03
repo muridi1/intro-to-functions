600b247b24add8a2e499620c429c6ae0eeaeb383
mohamed <muridi51@gmail.com>
2021-06-03 14:16:32 +0100

    challenge 0 js

---
+++ challenges/0-check-if-equal.js
@@ -0,0 +1,23 @@
+// function:
+// =========
+// name: isEqual
+// parameter a: number
+// parameter b: number
+// return: boolean
+
+
+function isEqual(a, b){
+    if (a === b) {
+        return true 
+    }else {
+        return false
+    }
+}
+
+// test cases:
+// ===========
+console.log(isEqual(4, 4), "should be True")
+console.log(isEqual(0, 0), "should be True")
+console.log(isEqual(-4, -4), "should be True")
+console.log(isEqual(4, -4), "should be False")
+console.log(isEqual(0, 1), "should be False")
