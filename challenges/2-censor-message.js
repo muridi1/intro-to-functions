c600a489392ceafd6e33955b2819f7a97f6ee12a
mohamed <muridi51@gmail.com>
2021-06-04 11:43:54 +0100

    last challenge

--- challenges/2-censor-message.js
+++ challenges/2-censor-message.js
@@ -1,38 +1,59 @@
-Write a function that, given a full message and a list of words, returns a single string that represents a 'censored' version of the original message: all words from the list must be scrubbed out and replaced with five asterisks (*****).
+// Write a function that, given a full message and a list of words, returns a single string that represents a 'censored' version of the original message: all words from the list must be scrubbed out and replaced with five asterisks (*****).
 
-The censor must scrub out matches that are case insensitive.
+// The censor must scrub out matches that are case insensitive.
 
-You may assume that each string in the list is a lowercase single word.
-You may assume that there is no punctuation apart from spaces in the full message.
+// You may assume that each string in the list is a lowercase single word.
+// You may assume that there is no punctuation apart from spaces in the full message.
 
-function:
-=========
-name: censorMessage
+// function:
+// =========
+// name: censorMessage
+// parameter: message: string
+// parameter: banList: list of strings
+// // return type: string
+// pseudo code:
+// ============
 
-parameter: message: string
-parameter: banList: list of strings
+function censorMessage(sentence,censorList){
+let rawWords = sentence.split(" ")
+let scrubbedWords = []
 
-return type: string
+rawWords.forEach((eachWord)=>{
+let lowercaseWord = eachWord.toLowerCase()
 
-pseudo code:
-============
-set rawWords to the list of strings created by separating the string message into sub-strings at every space (' ')
-
-set scrubbedWords to an empty list
-
-for each string word in rawWords
-	set lowercaseWord to lowercase(word)
-  if lowercaseWord is contained in the list banList
-    insert the string '*****' at the end of scrubbedWords
-  else
-    insert word at the end of scrubbedWords
-
-set scrubbedMessage to the string created by joining together every string in scrubbedWords with a space (' ') as a join character
-
+if (censorList.includes(lowercaseWord)){
+  scrubbedWords.push("*****")
+} else {
+  scrubbedWords.push(lowercaseWord)
+}
+})
+let scrubbedMessage = scrubbedWords.join(" ")
 return scrubbedMessage
 
-test cases:
-===========
-censorMessage("don't mess with cats", ["mess"]) should be "don't ***** with cats"
-censorMessage("Are you sure that this is safe", ["are", "is"]) should be "***** you sure that this ***** safe"
-censorMessage("I CANNOT HELP YOU", ["can", "not", "help", "you"]) should be "I CANNOT ***** *****"
+}
+
+//let str = "it is cold today"
+//str.split(" ")
+// ["it", "is", "cold", "today"]
+//str.join(" ")
+// "it is cold today"
+// set rawWords to the list of strings created by separating the string message into sub-strings at every space (' ')
+
+// set scrubbedWords to an empty list
+
+// for each string word in rawWords
+// 	set lowercaseWord to lowercase(word)
+//   if lowercaseWord is contained in the list banList
+//     insert the string '*****' at the end of scrubbedWords
+//   else
+//     insert word at the end of scrubbedWords
+
+// set scrubbedMessage to the string created by joining together every string in scrubbedWords with a space (' ') as a join character
+
+// return scrubbedMessage
+
+// test cases:
+// ===========
+console.log(censorMessage("don't mess with cats", ["mess"]), "should be don't ***** with cats")
+console.log(censorMessage("Are you sure that this is safe", ["are", "is"]), "should be ***** you sure that this ***** safe")
+console.log(censorMessage("I CANNOT HELP YOU", ["can", "not", "help", "you"]), "should be I CANNOT ***** *****")
